19fab0273b1abc47230fe8aa41930dd18beaaaaf
Chen-Sheng-Sheng <04360092@me.mcu.edu.tw>
2017-02-06 23:04:00 +0800

    Sheng菜單

---
+++ Sheng/菜單/菜單/src/菜單/Food.java
@@ -0,0 +1,14 @@
+package µæ³æ;
+
+import java.util.ArrayList;
+
+public class Food extends Item{
+
+	public Food(String name) {
+		super(name);
+	}
+	public Food(String name,ArrayList<Item> lowerName) {
+		super(name,lowerName);
+	}
+	
+}
