1b1444237b8d5f55c75b9d7c2f54bea0f380c191
林宗億 <tonylin601@gmail.com>
2017-02-20 14:24:52 +0800

    Update Man.java

--- LiNNN/基本多型知識-人(未完成)/src/Man.java
+++ LiNNN/基本多型知識-人(未完成)/src/Man.java
@@ -3,14 +3,16 @@ public class Man extends Human {
 
 	String[] tokens = str1.split("");
 	
+	@Override 
 	public void eat(){
 		for(int i = 0; i < str1.length(); i++){
 			if (tokens[i].equals('B') || tokens[i].equals('b')){
-				System.out.println("¦n¦n¦Y");
+				System.out.println("Â¦nÂ¦nÂ¦Y");
 			} else break;
 		}
 	}
 	
+	@Override 
 	public void sleep(){
 		for(int i = 0; i < str1.length(); i++){
 			if (tokens[i].equals('B') || tokens[i].equals('b')){
