966564ecbe928ea2b2a28a3d6727bdc5128253e4
Chengyiyu <ppoolkiu@gmail.com>
2017-02-28 18:32:12 +0800

    作業

---
+++ Chnegyi/多型/human/human/men.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace human
+{
+    class men :human
+    {
+        public override void eattalk()
+        {
+            Console.WriteLine("eat!eat!men");
+        }
+        public override void sleeptalk()
+        {
+            Console.WriteLine("sleep!sleep!men");
+        }
+
+
+
+    }
+}
---
+++ Chnegyi/學生/student/student/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入學生數量");
+             int iAmount;
+            iAmount = Convert.ToInt32(Console.ReadLine());
+            StudentData StuData = new StudentData();
+            StuData.amount = iAmount;
+            StuData.SetAmount();
+            StuData.input();
+            Console.Read();
+           
+        }
+        
+    }
+}
