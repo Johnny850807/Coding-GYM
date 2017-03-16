5e151eefd1175992aff6e84d85502910a086b81f
Maxyangen <blue203656629>
2017-03-16 22:39:39 +0800

    Push

--- Max/Shot(Factory-Method)/src/Hero.java
+++ Max/Shot(Factory-Method)/src/Hero.java
@@ -1,6 +1,5 @@
 
 public class Hero {
-	final private String name = "©¾©¾";
 	private Gun[] myGun = { new MachineGun() , new ExplosionGun(),new SniperGun()};
 	private int whichGun;
 	public Hero(){
