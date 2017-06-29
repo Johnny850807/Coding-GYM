ba0f62cad0264b1941055a708f1ab8ac15523935
ZYLiNN <tonylin601@gmail.com>
2017-06-30 02:54:24 +0800

    modify sort

--- LiNNN/物件村題目/src/學生2/StudentSystem.java
+++ LiNNN/物件村題目/src/學生2/StudentSystem.java
@@ -1,6 +1,8 @@
 package 學生2;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Comparator;
 
 public class StudentSystem {
         
@@ -14,19 +16,23 @@ public class StudentSystem {
                 System.out.println("請依序輸入身高、體重、成績、學號: ");
                 
                 for ( int i = 0; i < amount.length; i++ ) {
-                        //為什麼我這邊用 "input.nextLine();" 會沒辦法輸入ID啊?
-                        amount[i] = new StudentInformation( input.nextInt(), input.nextInt(), input.nextInt(), input.next() );
+                        amount[i] = new StudentInformation( input.nextInt(), input.nextInt(), input.nextInt(), input.nextLine() );
                 }
                 
-                for ( int a = amount.length-1; a >= 0; a-- ) {
-                        for ( int b = 0; b < a; b++ ) {
-                                if ( amount[b].getGrade() > amount[a].getGrade() ) {
-                                        StudentInformation tmp = amount[a];
-                                        amount[a] = amount[b];
-                                        amount[b] = tmp;
-                                } 
+                class CompareByGrade implements Comparator{
+                        @Override
+                        public int compare(Object o1, Object o2) {
+                                int grade1 = ((StudentInformation)o1).getGrade();
+                                int grade2 = ((StudentInformation)o2).getGrade();
+                                if ( grade1 > grade2)
+                                        return 1;
+                                if (grade1 < grade2)
+                                        return -1;
+                                return 0;
                         }
-                }
+                };
+                
+                Arrays.sort(amount, new CompareByGrade());
                 
                 for ( int i = 0; i < amount.length; i++ ) 
                         System.out.println( amount[i] );
