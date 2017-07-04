5f2cb9a0a21e092797038e6c443b349475629691
ZYLiNN <tonylin601@gmail.com>
2017-07-04 22:52:54 +0800

    多型(人)

---
+++ LiNNN/多型2/Woman.java
@@ -0,0 +1,19 @@
+package ¦h«¬2;
+
+public class Woman extends Human {
+
+        @Override
+        public void eat() {
+
+                System.out.println("Orzzz");
+                
+        }
+
+        @Override
+        public void sleep() {
+
+                System.out.println(":)))))))))");
+                
+        }
+
+}
