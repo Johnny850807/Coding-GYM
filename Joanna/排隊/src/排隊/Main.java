a780a324e7204e8ac1ce8948b340ade2bb9c52a7
xuan1280 <xuan1280@gmail.com>
2017-08-12 01:00:38 +0800

    更新

--- Joanna/排隊/src/排隊/Main.java
+++ Joanna/排隊/src/排隊/Main.java
@@ -1,5 +1,6 @@
 package 排隊;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -7,62 +8,66 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-	public static void main(String [] args) {
-		
-		Scanner input= new Scanner(System.in);
-		boolean isEnd = false;
-		
+	private static Scanner input= new Scanner(System.in);
+	private static String[] stores;
+	private static HashMap<String, ArrayList<String>> queue = new HashMap<>();
+	
+	public static void setStores() {
 		System.out.println("輸入商店：");
 		String line = input.nextLine();
-		String[] stores = line.split(" ");
-				
-		HashMap<String, String> queue = new HashMap<>();
-		
-		line = input.nextLine();
-		
-		while (!isEnd) {
-			
-			
-			String[] customerSituation = line.split(" ");
-			
-			
-			
-			if (customerSituation[2].equals("A"))
-				queue.put(customerSituation[1], customerSituation[0]);
-			else if (customerSituation[2].equals("R"))
-				queue.remove(customerSituation[1]);
-			
+		stores = line.split(" ");
+	}
+	
+	public static void queue() {
+		boolean isEnd = false;
+		String line = input.nextLine();
+		while (!isEnd) {	
+			increaseOrDeleteCustomer(line);
 			line = input.nextLine();
-			
 			if (line.equals("0")) {
 				isEnd = true;
 			}						
 		}
+	}
+	
+	public static void increaseOrDeleteCustomer(String line) {
+		String[] customerSituation = line.split(" ");
 		
+		if (customerSituation[2].equals("A"))
+			queue.get(customerSituation[0]).add(customerSituation[1]);
+		else if (customerSituation[2].equals("R"))
+			queue.get(customerSituation[0]).remove(customerSituation[1]);
+	}
+
+	public static void display() {
 		for (int i = 0; i < stores.length; i++) {
 			System.out.println("商店名稱：" + stores[i]);
 			System.out.print("隊伍：");
 			
-			Set set = new HashSet();
-			set =  queue.keySet();
-			Iterator it = set.iterator();
-			
-			String customer;
 			boolean noCustomer = true;
-			while (it.hasNext()) {
-				customer = it.next().toString();
-			    if (queue.get(customer).toString().equals(stores[i])) {
-			    	System.out.print(customer + " ");
-			    	noCustomer = false;
-			    }
-			}
 			
-			if (noCustomer)
+			for (int j = 0; j < queue.get(stores[i]).size(); j++)
+				System.out.print(queue.get(stores[i]).get(j) + " ");
+			
+			if (queue.get(stores[i]).isEmpty())
 				System.out.print("沒有人排隊");
 			
 			System.out.println();
 		}
-		
-		
 	}
+	
+	public static void main(String [] args) {		
+		setStores();
+		
+		for (int i = 0; i < stores.length; i++) {
+			ArrayList<String> customerList = new ArrayList();
+			queue.put(stores[i], customerList);
+		}
+				
+		queue();
+		display();
+
+	}
+	
+	
 }
