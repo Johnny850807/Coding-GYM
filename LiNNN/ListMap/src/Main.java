35808192525f459356f9815830e319138af3dfa6
ZYLiNN <tonylin601@gmail.com>
2018-09-11 13:42:20 +0800

    Data structure List & Hashmap

---
+++ LiNNN/ListMap/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        StoreManager storeManager = new StoreManager();
+
+        storeManager.customerLeaveOrGo();
+        storeManager.showStoreStatus();
+    }
+}
