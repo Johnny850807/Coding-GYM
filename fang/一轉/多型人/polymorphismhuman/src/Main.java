259bcbd05ed53325ca9dcd8a861af4145b73da09
zseqw123 <sakuya111235@gmail.com>
2022-06-09 17:04:09 +0800

    人修改

--- fang/一轉/多型人/polymorphismhuman/src/Main.java
+++ fang/一轉/多型人/polymorphismhuman/src/Main.java
@@ -2,21 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         Human man = new Man();
         Human woman = new Woman();
         String input = in.nextLine();
+        int length = input.length();
+        Human[] humans = new Human[length];
 
-        for (int i = 0; i < input.length(); i++) {
-            if(Character.toLowerCase(input.charAt(i)) == 'b'){
-                man.eat();
-                man.sleep();
-            }else if(Character.toLowerCase(input.charAt(i)) == 'g'){
-                woman.eat();
-                woman.sleep();
+        for (int i = 0; i < length; i++) {
+            char gender = Character.toLowerCase(input.charAt(i));
+            if (gender == 'b') {
+                humans[i] = man;
+            } else if (gender == 'g') {
+                humans[i] = woman;
             }
         }
 
+        for (Human human : humans) {
+            human.eat();
+            human.sleep();
+        }
     }
 }
