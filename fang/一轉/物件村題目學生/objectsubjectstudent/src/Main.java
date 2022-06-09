bfc978bcbef23acf67f7cec21c77458ab020f456
zseqw123 <sakuya111235@gmail.com>
2022-06-09 17:09:16 +0800

    學生修改

--- fang/一轉/物件村題目學生/objectsubjectstudent/src/Main.java
+++ fang/一轉/物件村題目學生/objectsubjectstudent/src/Main.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         int inputInt = in.nextInt();
         Student[] students = new Student[inputInt];
 
         for (int i = 0; i < students.length; i++) {
-            students[i] = new Student(in.nextInt(),in.nextInt(),in.nextInt()
-                    ,in.nextLine());
+            int height = in.nextInt();
+            int weight = in.nextInt();
+            int score = in.nextInt();
+            String studentId = in.nextLine();
+            students[i] = new Student(height, weight, score, studentId);
         }
 
         Student studentTemp;
         for (int i = 0; i < students.length - 1; i++) {
             for (int j = i; j < students.length - 1; j++) {
-                if(students[j].getScore() > students[j+1].getScore()){
+                if (students[j].getScore() > students[j + 1].getScore()) {
                     studentTemp = students[i];
-                    students[i] = students[j+1];
-                    students[j+1] = studentTemp;
+                    students[i] = students[j + 1];
+                    students[j + 1] = studentTemp;
                 }
             }
         }
 
-        for (Student s:students) {
+        for (Student s : students) {
             System.out.println(s);
         }
     }
